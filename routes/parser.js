14395764a11d71e08ab4be5033061097a88b065c
ted <itstedpark@gmail.com>
2015-06-28 00:13:01 +0900

    eq(0) 설명 첨부

--- routes/parser.js
+++ routes/parser.js
@@ -12,7 +12,7 @@ request({uri: 'http://cleanair.seoul.go.kr/air_city.htm?method=measure', encodin
         const $ 						= 	cheerio.load(iconv.convert(strContents).toString());
 
   			$('.tbl2 tbody tr').each(function() {
-
+				//eq(0) 괄호안의 숫자를 0~5 까지 순차적으로 지역, 미세먼지, 초미세먼지 순으로 받아오게 됩니다.
 					let strArea 	= 	$(this).find("td").eq(0).text().replace(/\s+/, "");
 					strArea 			= 	strArea.replace(/(\r\n|\n|\r)/gm,"");
           strArea 			= 	strArea.replace(/\s+/, "");
